863a1f12ea38cc7c09d7a927646d509929bcc5c6
Antix221 <61344834+Antix221@users.noreply.github.com>
2020-02-26 21:57:02 +0300

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define N 5
+#define M 5
+int main()             /*группа 1142 Ермаков Алексей*/
+{
+	int array[N][M];
+	int i, j;
+	for (i = 0; i < N; ++i) {
+		for (j = 0; j < M; ++j) {
+			if (i == 0 || j == 0) {
+				array[i][j] = 1;
+			}
+			else
+				array[i][j] = rand() % 2;
+			if (i % (N - 1) == 0) {
+				array[i][j] = 1;
+			}
+			else if (j % (M - 1) == 0) {
+				array[i][j] = 1;
+			}
+		}
+	}
+	int pat = 0;
+	for (i = 0; i < N; ++i) {
+		for (j = 0; j < M; ++j) {
+			printf("%d", array[i][j]);
+			printf(" ");
+			++pat;
+			if (pat == M) {
+				printf("\n");
+				pat = 0;
+			}
+		}
+	}
+}
